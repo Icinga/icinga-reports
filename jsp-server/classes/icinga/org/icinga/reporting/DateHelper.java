df3a43a5ad06f7526365ef45fe20c21d4dd93c74
Bernd Erk <bernd.erk@icinga.org>
2013-10-17 10:59:35 +0200

    Add new quarter function from patch fixes #3718. Thanks to dirk for providing the patch.

--- jsp-server/classes/icinga/org/icinga/reporting/DateHelper.java
+++ jsp-server/classes/icinga/org/icinga/reporting/DateHelper.java
@@ -8,10 +8,12 @@ import java.util.Date;
  * Class helping jasper to generate reports for the following periods 
  * - last week 
  * - last month 
+ * - last quarter 
  * - last year
  * 
- * @version 1.3
+ * @version 1.4
  * @author berk
+ * @author dgoetz
  * 
  */
 public class DateHelper {
@@ -22,6 +24,8 @@ public class DateHelper {
 		System.out.println("Last-Week End: " + getLastWeekEnd());
 		System.out.println("Last-Month Start: " + getLastMonthStart());
 		System.out.println("Last-Month End: " + getLastMonthEnd());
+		System.out.println("Last-Quarter Start: " + getLastQuarterStart());
+		System.out.println("Last-Quarter End: " + getLastQuarterEnd());
 		System.out.println("Last-Year Start: " + getLastYearStart());
 		System.out.println("Last-Year End: " + getLastYearEnd());
 	}
@@ -81,6 +85,64 @@ public class DateHelper {
 		Timestamp ts = new Timestamp(cal.getTime().getTime());
 		return ts;
 	}
+
+	static public Timestamp getLastQuarterStart() {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		switch(cal.get(Calendar.MONTH))
+		{
+			case Calendar.JANUARY: case Calendar.FEBRUARY: case Calendar.MARCH:
+				cal.add(Calendar.YEAR, -1);
+				cal.set(Calendar.MONTH, Calendar.OCTOBER);
+				break;
+			case Calendar.APRIL: case Calendar.MAY: case Calendar.JUNE:
+				cal.set(Calendar.MONTH, Calendar.JANUARY);
+				break;
+			case Calendar.JULY: case Calendar.AUGUST: case Calendar.SEPTEMBER:
+				cal.set(Calendar.MONTH, Calendar.APRIL);
+				break;
+			case Calendar.OCTOBER: case Calendar.NOVEMBER: case Calendar.DECEMBER:
+				cal.set(Calendar.MONTH, Calendar.JULY);
+				break;
+		}
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		Timestamp ts = new Timestamp(cal.getTime().getTime());
+		return ts;
+	}	
+
+	static public Timestamp getLastQuarterEnd() {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		switch(cal.get(Calendar.MONTH))
+		{
+			case Calendar.JANUARY: case Calendar.FEBRUARY: case Calendar.MARCH:
+				cal.add(Calendar.YEAR, -1);
+				cal.set(Calendar.MONTH, Calendar.DECEMBER);
+				break;
+			case Calendar.APRIL: case Calendar.MAY: case Calendar.JUNE:
+				cal.set(Calendar.MONTH, Calendar.MARCH);
+				break;
+			case Calendar.JULY: case Calendar.AUGUST: case Calendar.SEPTEMBER:
+				cal.set(Calendar.MONTH, Calendar.JUNE);
+				break;
+			case Calendar.OCTOBER: case Calendar.NOVEMBER: case Calendar.DECEMBER:
+				cal.set(Calendar.MONTH, Calendar.SEPTEMBER);
+				break;
+		}
+		cal.add(Calendar.MONTH, 1);
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.add(Calendar.DAY_OF_MONTH, -1);
+		cal.set(Calendar.HOUR_OF_DAY, 23);
+		cal.set(Calendar.MINUTE, 59);
+		cal.set(Calendar.SECOND, 59);
+		cal.set(Calendar.MILLISECOND, 0);
+		Timestamp ts = new Timestamp(cal.getTime().getTime());
+		return ts;
+	}	
 
 	static public Timestamp getLastYearStart() {
 		Calendar cal = Calendar.getInstance();
